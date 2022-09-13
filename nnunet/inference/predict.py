7fc0865f6f3367016dc4ede94b7c9cc935609f4c
cciausu <72577931+ccosmin97@users.noreply.github.com>
2022-09-13 14:06:51 -0400

    Update predict.py
    disable sliding window and use gaussian

--- nnunet/inference/predict.py
+++ nnunet/inference/predict.py
@@ -214,15 +214,15 @@ def predict_cases(model, list_of_lists, output_filenames, folds, save_npz, num_t
         print("predicting", output_filename)
         trainer.load_checkpoint_ram(params[0], False)
         softmax = trainer.predict_preprocessed_data_return_seg_and_softmax(
-            d, do_mirroring=do_tta, mirror_axes=trainer.data_aug_params['mirror_axes'], use_sliding_window=True,
-            step_size=step_size, use_gaussian=True, all_in_gpu=all_in_gpu,
+            d, do_mirroring=do_tta, mirror_axes=trainer.data_aug_params['mirror_axes'], use_sliding_window=False,
+            step_size=step_size, use_gaussian=False, all_in_gpu=all_in_gpu,
             mixed_precision=mixed_precision)[1]
 
         for p in params[1:]:
             trainer.load_checkpoint_ram(p, False)
             softmax += trainer.predict_preprocessed_data_return_seg_and_softmax(
-                d, do_mirroring=do_tta, mirror_axes=trainer.data_aug_params['mirror_axes'], use_sliding_window=True,
-                step_size=step_size, use_gaussian=True, all_in_gpu=all_in_gpu,
+                d, do_mirroring=do_tta, mirror_axes=trainer.data_aug_params['mirror_axes'], use_sliding_window=False,
+                step_size=step_size, use_gaussian=False, all_in_gpu=all_in_gpu,
                 mixed_precision=mixed_precision)[1]
 
         if len(params) > 1:
